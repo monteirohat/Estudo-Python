3acf02eec60fc8fcda813c78de93b4cee3bf9fbd
Andre Monteiro <monteiro.hat@gmail.com>
2021-01-23 09:41:06 -0300

    Adicionado os arquivos de estudo

---
+++ Mad Libs Generator/main.py
@@ -0,0 +1,10 @@
+#----------------------------------------------------------
+# Mad Libs: É um jogo em que se substitui as palavras de 
+# uma historia, gerando uma história engraçada e divertida
+#----------------------------------------------------------
+
+#1° - Menu de escolha das histórias
+
+
+loop = 1
+while (loop < 10):
